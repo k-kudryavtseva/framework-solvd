54296cc7959b40083927418c11ef408b66e3db2b
Kristina Kudryavtseva <kkudryavtseva@Kristinas-Mac-mini.local>
2021-02-10 17:41:37 +0300

    Params for the lighthouse command were added.

--- cypress/integration/excel.spec.js
+++ cypress/integration/excel.spec.js
@@ -3,10 +3,24 @@ const testData = require('../fixtures/testData.json')
 testData.forEach((testDataRow, index, testDataArray) => {
     describe('Test urls from excel', () => {
         beforeEach('testing url', () => {
+
+            // By default, if you don’t provide any arguments to the lighthouse command,
+            // the test will fail if any of the categories have a reported score of less than 100.
+            const customThresholds = {
+                performance: 0,
+                accessibility: 0,
+                seo: 0,
+            };
+
+            // By default, if you don’t provide any arguments to the lighthouse command,
+            // your page will be simulated on mobile view.
+            const desktopConfig = {
+                formFactor: 'desktop',
+                screenEmulation: {disabled: true},
+            };
+
             cy.visit(testDataRow.url)
-            cy.lighthouse({
-                performance: 60
-            })
+            cy.lighthouse(customThresholds, desktopConfig)
         })
         it('',() => {
 					const report = {
